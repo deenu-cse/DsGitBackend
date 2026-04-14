633a530697f53eafab528b8d7af07133220ca8c6
deenu-cse <236301074@gkv.ac.in>
2026-04-14 12:51:20 +0530

    solve mongodb bug

--- server.js
+++ server.js
@@ -29,12 +29,9 @@ app.use(cors({
 
 // Connect to MongoDB
 if (process.env.MONGO_URI) {
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log('MongoDB Connection Error:', err));
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('MongoDB Connection Error:', err));
 } else {
   console.log("No MONGO_URI provided in .env");
 }
